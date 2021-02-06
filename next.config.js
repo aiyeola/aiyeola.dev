bad4eec833f50a7d269015d65e38700b096967d6
Victor Aiyeola <aiyeolavictor@gmail.com>
2021-02-06 12:03:28 +0100

    - blog posts

--- next.config.js
+++ next.config.js
@@ -3,4 +3,21 @@ module.exports = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+  webpack: (config, { dev, isServer }) => {
+    if (isServer) {
+      require('./scripts/generate-sitemap');
+      // require('./scripts/generate-rss');
+    }
+
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
 };
