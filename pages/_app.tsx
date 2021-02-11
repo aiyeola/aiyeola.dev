f40cc12040de743ae7d420059696da1be5d470e5
Victor Aiyeola <aiyeolavictor@gmail.com>
2021-02-11 22:30:41 +0100

    - add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -31,6 +31,25 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <TopProgressBar />
       <ThemeProvider theme={theme}>
