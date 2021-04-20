d8f83d9e56643eea9fe7dd0f1f3baa350ae1b09b
Victor Aiyeola <aiyeolavictor@gmail.com>
2021-04-20 09:17:10 +0100

    - next bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   webpack: (config, { isServer }) => {
     if (isServer) {
@@ -7,4 +11,4 @@ module.exports = {
     }
     return config;
   },
-};
+});
